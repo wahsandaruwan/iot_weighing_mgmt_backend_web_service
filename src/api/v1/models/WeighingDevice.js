de5646bc5b7118ddc96e0b67b8e5ef372a9e8cdf
Prabodashan <prabodashan14@gmail.com>
2023-12-18 00:00:04 +0530

    Added query to filter data daily weekly and monthly

--- src/api/v1/models/WeighingDevice.js
+++ src/api/v1/models/WeighingDevice.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 // ----------User schema----------
 const WeighingDeviceSchema = new mongoose.Schema(
   {
-   
     title: {
       type: String,
       required: true,
